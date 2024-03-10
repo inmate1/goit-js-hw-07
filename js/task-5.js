fb95c682323efe7d403bd33361e3597ce275505e
Aleksander <aleksander.0705@gmail.com>
2024-03-10 21:34:02 +0200

    add task-5 and task-6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const colorBody = document.querySelector('body');
+const outputColor = document.querySelector('.color');
+btn.addEventListener('click', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  colorBody.style.backgroundColor = getRandomHexColor();
+  outputColor.innerHTML = `<span class="color">${getRandomHexColor()}</span>`;
+}
+
+const hoverBtn = function () {
+  btn.style.backgroundColor = '#4e75ff';
+  btn.onmouseover = () => {
+    btn.style.backgroundColor = '#6c8cff';
+  };
+  btn.onmouseout = () => {
+    btn.style.backgroundColor = '#4e75ff';
+  };
+};
+hoverBtn();
+
+// btn.addEventListener('blur', handleBlur);
+// function handleBlur(event) {
+//   colorBody.removeAttribute("style");
+//   outputColor.innerHTML = `<span class="color">-</span>`;
+// }
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,51 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+input.addEventListener('input', handleInput);
+createBtn.addEventListener('click', handleCreate);
+destroyBtn.addEventListener('click', handleDestroy);
+
+let amount;
+function handleInput(event) {
+  amount = input.value;
+}
+
+function handleCreate(event) {
+  handleDestroy();
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+  input.value = '';
+  amount = '';
+}
+
+function handleDestroy() {
+  boxes.innerHTML = '';
+}
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const boxElement = `
+       <div class="box-item" style= "background-color: ${getRandomHexColor()}; width: ${boxSize}px; height:${boxSize}px; margin-right: 16px"></div>`;
+    boxes.insertAdjacentHTML('afterbegin', boxElement);
+
+    boxSize += 10;
+  }
+}
+
+const hoverBtn = function () {
+  destroyBtn.style.backgroundColor = '#ff4e4e';
+  destroyBtn.onmouseover = () => {
+    destroyBtn.style.backgroundColor = '#ff7070';
+  };
+  destroyBtn.onmouseout = () => {
+    destroyBtn.style.backgroundColor = '#ff4e4e';
+  };
+};
+hoverBtn();
